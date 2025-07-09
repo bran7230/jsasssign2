c309627f644569f87bc9bc36c71f2be68dd63faf
Brandon Trumble <brandon.ta.trumble@icloud.com>
2025-07-09 04:05:39 -0400

    added more styles and added more elements to page aswell as adjusted constructor

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@
 let smoothieForm = document.getElementById("smoothieForm");
 let output = document.getElementById("output");
 
+
 /*
  * Smoothie Class
  * Represents a smoothie with a quantity and fruit type.
@@ -15,18 +16,27 @@ class Smoothie{
 
     quantity;
     fruit;
+    size;
+    liquid;
+    sweetener;
 
-    constructor(quantity, fruit) {
+    constructor(quantity, fruit, size, liquid, sweetener) {
         if(quantity < 1 || quantity > 10) {
             throw new Error("Quantity must be between 1 and 10");
         }
        
         this.quantity = quantity;
         this.fruit = fruit;
+        this.size = size;
+        this.liquid = liquid;
+        this.sweetener = sweetener;
     }
 
     getSmoothieDetails(){
-        return `Smoothie with ${this.quantity} servings of ${this.fruit}`;
+        if(this.sweetener == "none"){
+            return `A ${this.size} ${this.liquid} Smoothie with ${this.quantity} servings of ${this.fruit}.`;
+        }
+        return `A ${this.size} ${this.liquid} Smoothie with ${this.quantity} servings of ${this.fruit} and ${this.sweetener} sweetener.`;
     }
 
 }
@@ -39,9 +49,12 @@ smoothieForm.addEventListener("submit", function(event) {
 
     let quantity = parseInt(document.getElementById("quantity").value);
     let fruit = document.getElementById("fruit").value;
+    let size = document.getElementById("size").value;
+    let liquid = document.getElementById("liquid").value;
+    let sweetener = document.getElementById("sweetener").value;
 
     try{
-        let smoothie = new Smoothie(quantity, fruit);
+        let smoothie = new Smoothie(quantity, fruit, size, liquid, sweetener);
         output.textContent = smoothie.getSmoothieDetails();
     }
     catch(error) {
